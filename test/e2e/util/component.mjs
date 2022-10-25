0269c3102d0eb4894a8a940ee54db5c788711c02
Matt Levy <ducksoupdev@gmail.com>
2022-10-25 18:01:01 +0100

    feat: generate sourcemaps

--- test/e2e/util/component.mjs
+++ test/e2e/util/component.mjs
@@ -1,4 +1,4 @@
-import { renderer, html } from 'https://cdn.skypack.dev/@ficusjs/renderers@3/uhtml'
+import { renderer, html } from 'https://cdn.skypack.dev/@ficusjs/renderers@5/uhtml'
 import { createCustomElement as customElementCreator } from 'https://cdn.skypack.dev/@ficusjs/core'
 import { withLocalState } from 'https://cdn.skypack.dev/@ficusjs/state'
 import { createEventBus, getEventBus, withEventBus } from '../../../src/index.mjs'
